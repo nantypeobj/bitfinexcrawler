1bc68e062abda89773a1fce071c6a09b4ad51cc2
nantypeobj <warriorzhai@126.com>
2018-09-06 20:00:10 +0800

    新增okex实时数据更新模块

--- updatekline.py
+++ updatekline.py
@@ -6,36 +6,66 @@ Created on Sun Sep  2 13:34:43 2018
 """
 import sys
 from  BitfinexAPI import BitfinexAPI
+from  OKCoinFuture import OKCoinFuture
 from DatabaseInterface import DatabaseInterface
 import base
 import datetime
 import time
 
-#BITFINEX：获取K线
-def getkline(pair,timeframe,start,end=None):
+
+def initdata_from_okex(pair,timeframe,usedb):
+    db=DatabaseInterface(usedb)
+    api=OKCoinFuture()
+    kline=api.future_kline(pair,'quarter',timeframe=timeframe,period={'months':-3},size=5000)
+    if not kline.empty:
+        kline=kline[['high','low','open','close','time','volume','pct_change']]
+        kline['pair']=pair.replace('_','').upper()
+        db.db_insertdataframe(kline,'KLINE'+timeframe)
+        return True
+    else:
+        return False
+
+def getkline_from_bitfinex(pair,timeframe,exchangename,start,end=None):
     api=BitfinexAPI()
     if pair[0]!='t':
         pair='t'+pair
     res=api.klines(pair,timeframe,start=start,end=end)
-#    if not res is None:
-        #*为了避免在数据存储过程中timestamp的自动转换问题，将时间作为str保存
-#        res['time']=[str(base.datetime_toTimestamp(t)) for t in res.index]
     return res
+
+def getkline_from_okex(pair,timeframe,exchangename,start):
+    api=OKCoinFuture()
+    kline=api.future_kline(pair,'quarter',timeframe=timeframe,since=start,size=5000)
+    if not kline.empty:
+        kline=kline[['high','low','open','close','time','volume','pct_change']]
+        kline['pair']=pair.replace('_','').upper()
+    return kline
+
+#BITFINEX：获取K线
+def getkline(pair,timeframe,exchangename,start,end=None):
+    if exchangename=='bitfinex':
+        return getkline_from_bitfinex(pair,timeframe,exchangename,start,end)
+    elif exchangename=='okex':
+        return getkline_from_okex(pair,timeframe,exchangename,start)
+    else:
+        print('输入的交易所不存在')
 
 
 #更新已有的k线数据到最新
-def updateklinedata(pair,timeframe,usedb):
+def updateklinedata(pair,timeframe,exchangename,usedb):
     #取当前数据库中最晚的一条数据的时间加上一秒为起始，获取到当前时间的数据#
     
     db=DatabaseInterface(usedb)
     lastrow=db.db_findone('KLINE'+timeframe,filter_dic={'pair':pair},sel_fields=[],sort=[("time", -1)])
     if lastrow is None:
-        assert False,'数据库中没有找到数据'
+        if exchangename=='okex':
+            return initdata_from_okex(pair,timeframe,usedb)
+        else:
+            assert False,'数据库中没有找到数据'
     lasttime=base.timestamp_toStr(float(lastrow['time'])+1,dateformat="%Y%m%d %H:%M:%S")
     #交易所获取k线函数
    # print(lasttime)
    # print(str(lastrow['time']))
-    df=getkline(pair,timeframe,lasttime,end=None)
+    df=getkline(pair,timeframe,exchangename,lasttime,end=None)
     print (df.shape[0])
 #    df=None
      #没有新数据，不更新
@@ -52,16 +82,22 @@ def updatesignal(pair,timeframe,usedb):
     db.db_updateone({'timeframe':timeframe,'pair':pair},{'sig':'1'},'DATAUPDATESIG',upserts=True)
 
 if __name__ == '__main__':
-    usedb='data_bitfinex'
+    #输入值
+    #sys.argv[1]，pair，如：BTCUSD,EOSUSD,btc_usd....
+    #sys.argv[2]，timeframe，如：30m,1hour....
+    #sys.argv[3]，exchangename,bitfinex，okex
+
+    #
+    usedb='data_'+sys.argv[3].lower()
 #    pair,timeframe='EOSUSD','30m'
     time.sleep(30)
-    flg=updateklinedata(sys.argv[1],sys.argv[2],usedb)
+    flg=updateklinedata(sys.argv[1],sys.argv[2],sys.argv[3],usedb)
     print('updating date at time'+str(datetime.datetime.now()))
     if flg: 
-    	print('data updated at time'+str(datetime.datetime.now()))
+        print('data updated at time'+str(datetime.datetime.now()))
         updatesignal(sys.argv[1],sys.argv[2],usedb)
     else:
-        print('empty data')
+       print('empty data')
     #flg=updateklinedata('BTCUSD','1D',usedb)
 
   
